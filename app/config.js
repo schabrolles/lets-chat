49a83c036bbaefc5c7d0719c9ab7fefa80dcb1a2
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2020-04-07 09:38:52 +0200

    update variable

--- app/config.js
+++ app/config.js
@@ -170,7 +170,7 @@ var pipeline = [
                                           // process.env.OPENSHIFT_APP_NAME;
             
             // context.result.database.uri = "mongodb://"+process.env.username+":"+process.env.password+"@"+process.env.MONGODB_PORT_27017_TCP_ADDR+":"+process.env.MONGODB_SERVICE_PORT+"/"+process.env.database_name;
-            context.result.database.uri = "mongodb://admin:"+process.env.database-admin-password+"@"+process.env.MONGODB_PORT_27017_TCP_ADDR+":"+process.env.MONGODB_SERVICE_PORT+"/"+process.env.database_name+"?authSource=admin";
+            context.result.database.uri = "mongodb://admin:"+process.env.'database-admin-password'+"@"+process.env.MONGODB_SERVICE_HOST+":"+process.env.MONGODB_SERVICE_PORT+"/"+process.env.'database_name'+"?authSource=admin";
             console.log(context.result.database.uri) 
         }
     }
